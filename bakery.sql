273db6e190353d267cb15b0622bf52fc54897768
LwinMoeAung <53222794+Lwinmoeaung-slientcoder@users.noreply.github.com>
2019-07-23 19:49:45 +0630

    Add files via upload

---
+++ bakery.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jul 23, 2019 at 03:19 PM
+-- Server version: 10.3.16-MariaDB
+-- PHP Version: 7.3.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bakery`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inventory`
+--
+-- Error reading structure for table bakery.inventory: #1932 - Table 'bakery.inventory' doesn't exist in engine
+-- Error reading data for table bakery.inventory: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `bakery`.`inventory`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `item_order`
+--
+-- Error reading structure for table bakery.item_order: #1932 - Table 'bakery.item_order' doesn't exist in engine
+-- Error reading data for table bakery.item_order: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `bakery`.`item_order`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+-- Error reading structure for table bakery.login: #1932 - Table 'bakery.login' doesn't exist in engine
+-- Error reading data for table bakery.login: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `bakery`.`login`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sale`
+--
+-- Error reading structure for table bakery.sale: #1932 - Table 'bakery.sale' doesn't exist in engine
+-- Error reading data for table bakery.sale: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `bakery`.`sale`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_comment`
+--
+-- Error reading structure for table bakery.user_comment: #1932 - Table 'bakery.user_comment' doesn't exist in engine
+-- Error reading data for table bakery.user_comment: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `bakery`.`user_comment`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
